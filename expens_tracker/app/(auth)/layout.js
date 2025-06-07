e8cf0c427633027bb0231c512531213219523722
Aman kumar <iit2022168@iiita.ac.in>
2025-06-08 04:04:22 +0530

    add_pages

---
+++ expens_tracker/app/(auth)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthLayoout = ({children}) => {
+  return (
+    <div className="flex justify-center pt-40 "> {children} </div>
+  );
+};
+
+export default AuthLayoout;
--- expens_tracker/app/page.js
+++ expens_tracker/app/page.js
@@ -1,6 +1,12 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
-  return <Button variant="destructive">subcribe aman</Button>
+  return(
+    <div className="mt-40">
+         <HeroSection />
+
+    </div>
+  );
 }
