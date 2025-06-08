8cf43661a787079cbc1d00e8d5a4a416708aff0a
Aman kumar <iit2022168@iiita.ac.in>
2025-06-08 06:53:04 +0530

    allright

---
+++ expens_tracker/app/(main)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-2">Dashboard</h1>
+      <p className="text-gray-600">Welcome to your dashboard!</p>
+    </main>
+  );
+};
+
+export default Dashboard;
