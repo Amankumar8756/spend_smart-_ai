3d6498b36104fb125c91582095d3a77e39a6312f
Aman kumar <iit2022168@iiita.ac.in>
2025-06-16 17:50:49 +0530

    check_inngest

---
+++ expens_tracker/lib/inngest/functions.js
@@ -0,0 +1,11 @@
+// inngest/functions/helloWorld.js
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait", "1s");
+    return { message: `Hello ${event.data.email}!` };
+  }
+);
