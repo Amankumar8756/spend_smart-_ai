cc9f2e4b23ae472a58020c378b857211939d9b65
Aman kumar <iit2022168@iiita.ac.in>
2025-06-19 19:15:07 +0530

    generate_andedit_correct

--- expens_tracker/app/api/inngest/route.js
+++ expens_tracker/app/api/inngest/route.js
@@ -1,13 +1,18 @@
-import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
+import {
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+  generateMonthlyReports,
+  checkBudgetAlerts,
+} from "@/lib/inngest/functions";
 
-
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
+    processRecurringTransaction,
+    triggerRecurringTransactions,
+    generateMonthlyReports,
     checkBudgetAlerts,
   ],
 });
