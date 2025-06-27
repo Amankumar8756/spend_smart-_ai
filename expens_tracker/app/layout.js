0442c82f5d53f309125e6f952d11ef72022317ca
Aman kumar <iit2022168@iiita.ac.in>
2025-06-27 19:23:53 +0530

    project-D

--- expens_tracker/app/layout.js
+++ expens_tracker/app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Welth",
+  title: "Expence_Tracker_Using_AI",
   description: "One stop Finance Platform",
 };
 
